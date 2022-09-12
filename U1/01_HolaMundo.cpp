993024872917188423fa283d8fbc471ac1c27f57
UP10300 <up210300@alumnos.upa.edu.mx>
2022-09-12 16:42:22 -0500

    First program

---
+++ U1/01_HolaMundo.cpp
@@ -0,0 +1,22 @@
+/*
+Date: 12/09/2022
+Author: Sofia Calderon Juarez
+Email: up210300@alumnos.upa.edu.mx
+Description: Mi primer programa en C++ 
+Last Modification:
+*/
+
+//-----PREPROCESSOR DIRECTIVES-----
+
+//Inpunt/Outut library between the screen and the keyboard
+#include <iostream>
+
+
+//-----MAIN FUNCTION-----
+int main(){
+
+    std::cout << "Hello world, This is my first rogram";  
+
+    //Return 0 means that the program ends succesfully
+    return 0;
+}
--- U1/Test.cpp
+++ U1/Test.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 int main(){
 
-    cout << "Hola";
+    cout << "Hola" << endl;
 
     return 0;
 }
