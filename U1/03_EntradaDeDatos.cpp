1c7bfacca86eae0e15383ad08290b47c2287b13f
UP10300 <up210300@alumnos.upa.edu.mx>
2022-09-22 00:25:14 -0500

    Exercise

---
+++ U1/03_EntradaDeDatos.cpp
@@ -0,0 +1,40 @@
+/*
+    Unit 1. Entry of data
+    Autor: Sofia Calderon Juarez
+    Date: 21/09/2022
+    Description: Ways to entry different types of data by the user
+*/
+
+//Library for output and input of the screen
+#include <iostream>
+//LIbrary for the use of printf and scanf
+#include <stdio.h>
+
+//Use of namespace to avoid the use of std::
+using namespace std;
+
+//Main function integer type
+int main (){
+    //More info at: 
+    //https://www.geeksforgeeks.org/c-data-types/
+    int entero;
+    float flotante;
+    double grande;
+    char caracter;
+
+    cout << "Este programa muestra el ingreso de datos para el susario. \n";
+    cout << "Ingresa un dato de tipo entero: ";
+    cin >> entero;
+    cout << "El dato que ingreso es: " << entero << endl;
+    
+    //More info at:
+    //https://cplusplus.com/reference/cstdio/printf/
+    printf ("Usando la funcion printf y scanf\n");
+    printf ("Ingresa un dato de tipo entero: \n");
+    scanf ("%", &entero);
+    printf ("El dato ingresado es: %i \n", entero);
+
+    //As a function it must return to a value, in this case 0
+    return 0;
+
+    }
