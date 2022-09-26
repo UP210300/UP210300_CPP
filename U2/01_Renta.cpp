c4de0f3b2c7122b078def32ae03353984345f476
UP10300 <up210300@alumnos.upa.edu.mx>
2022-09-26 17:01:20 -0500

    Cambios

--- U2/01_Renta.cpp
+++ U2/01_Renta.cpp
@@ -22,33 +22,35 @@ int main (){
     cout << "The tax brackets for the declaration of income in a given country are the following: \nIncome                             Tax rate \nLess than   $10,000                 5% \nBetween     $10,000 and $20,000     15% \nBetween     $20,000 and $35,000     20% \nBetween     $35,000 and $60,000     30% \nMore than   $60,000                 45% " << endl;
 
     float income, taxToPay;
+    int percentage;
+
+    //Ask the user for their income
     cout << "Please enter your annual rent: ";
     cin >>income;
+    
+    
+    //Look for the tax rate acording to the income
     if (income<10000){
-        cout << "You have to pay 5% of taxes \n";
-        taxToPay=income*1.05;
-        cout << "You have to pay " <<taxToPay;
+        percentage=5;
     }
     else if (income>=10000 && income<20000){
-        cout << "You have to pay 15% of taxes \n";
-        taxToPay=income*1.15;
-        cout << "You have to pay " <<taxToPay;
+        percentage=15;
     }
     else if (income>=20000 && income<35000){
-        cout << "You have to pay 20% of taxes \n";
-        taxToPay=income*1.2;
-        cout << "You have to pay " <<taxToPay;
+        percentage=20;
     }
     else if (income>=35000 && income<60000){
-        cout << "You have to pay 30% of taxes \n";
-        taxToPay=income*1.3;
-        cout << "You have to pay " <<taxToPay;
+        percentage=30;
     }
     else if (income>=60000){
-        cout << "You have to pay 45% of taxes \n";
-        taxToPay=income*1.45;
-        cout << "You have to pay " <<taxToPay;
+        percentage=45;
     }
+    
+    
+    //Operation of how much the user have to pay acording to the tax rate and print the result
+    taxToPay=income*percentage/100;
+    cout << "Your tax rate is: " <<percentage;  cout << "%\n";
+    cout << "You have to pay: " <<taxToPay;  cout << " $" <<endl;
 
     //As a function it must return to a value, in this case 0
     return 0;
