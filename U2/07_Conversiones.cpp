480c7175ddb44a1ff2ea75b1a12365f6416cbe38
UP10300 <up210300@alumnos.upa.edu.mx>
2022-10-11 23:59:23 -0500

    Exercise

---
+++ U2/07_Conversiones.cpp
@@ -0,0 +1,22 @@
+/*
+    Unit 2. Conditionals
+    Author: Sofia Calderon Juarez
+    Date: 03/10/2022
+    Description: Shows the use of do while
+*/
+
+//Library for output and input of the screen
+#include <iostream>
+//LIbrary for the use of printf and scanf
+#include <stdio.h>
+
+//Use of namespace to avoid the use of std::
+
+using namespace std;
+
+//Program that converts a binary number to a decimal number
+
+//Main function integer type
+int main (){
+
+}
